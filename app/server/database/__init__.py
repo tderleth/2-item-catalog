1dda0962aba9c62a74b5f892f075ec03d1fb2157
tderleth <thomas.derleth@me.com>
2018-11-21 19:12:24 +0100

    fix: get config from current app context

--- app/server/database/__init__.py
+++ app/server/database/__init__.py
@@ -3,11 +3,12 @@
 
 """Database package."""
 
+from flask import current_app
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'], convert_unicode=True)
+engine = create_engine(current_app.config['SQLALCHEMY_DATABASE_URI'], convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
